1995a41f4f1b72f29549beb63009783510e56824
Sjoerd Visscher <sjoerd@w3future.com>
2018-07-21 13:07:38 +0200

    First results

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -funbox-strict-fields #-}
+{-# OPTIONS_GHC -funbox-strict-fields -O2 #-}
 {-# LANGUAGE BinaryLiterals, GeneralizedNewtypeDeriving #-}
 module Main where
 
@@ -13,6 +13,7 @@ import Control.Monad (unless)
 import Control.Monad.State.Strict
 import Control.Monad.Writer.Strict
 import Control.Monad.Except
+import Debug.Trace
 
 data Coord = Coord !Int16 !Int16 !Int16 deriving (Show, Eq, Ord)
 type Model = Set Coord
@@ -23,9 +24,10 @@ data ND = ND !Int16 !Int16 !Int16 deriving (Show, Eq, Ord)
 data Command = Halt | Wait | Flip | SMove !LLD | LMove !SLD !SLD | FusionP !ND | FusionS !ND | Fission !ND !Int16 | Fill !ND deriving (Show, Eq, Ord)
 data Harmonics = High | Low deriving (Show, Eq, Ord, Enum)
 data S = S 
-  { energy :: Int
+  { energy :: Integer
   , harmonics :: Harmonics
   , matrix :: Model
+  , todo :: Model
   , bots :: [Bot]
   } deriving (Show, Eq)
 data Bot = Bot 
@@ -37,11 +39,11 @@ data Bot = Bot
 newtype Builder a = Builder { unBuilder :: WriterT S.Put (StateT S (Except String)) a }
   deriving (Functor, Applicative, Monad, MonadWriter S.Put, MonadState S, MonadError String)
 
-runBuilder :: Builder () -> Either String S.Put
-runBuilder = fmap (snd . fst) . runExcept . flip runStateT s_init . runWriterT . unBuilder
+runBuilder :: Model -> Builder () -> Either String S.Put
+runBuilder model = fmap (snd . fst) . runExcept . flip runStateT (s_init model) . runWriterT . unBuilder
 
-s_init :: S
-s_init = S 0 Low Set.empty [Bot 1 (Coord 0 0 0) [2..20]]
+s_init :: Model -> S
+s_init model = S 0 Low Set.empty model [Bot 1 (Coord 0 0 0) [2..20]]
 
 putAsW8 :: Int16 -> S.Put
 putAsW8 = S.putWord8 . toEnum . fromEnum
@@ -73,6 +75,9 @@ straight Z dz (Coord x y z) = Coord x y (z + dz)
 addND :: ND -> Coord -> Coord
 addND (ND dx dy dz) (Coord x y z) = Coord (x + dx) (y + dy) (z + dz)
 
+diffND :: Coord -> Coord -> ND
+diffND (Coord x1 y1 z1) (Coord x2 y2 z2) = ND (x1 - x2) (y1 - y2) (z1 - z2)
+
 out :: Command -> Builder ()
 out = tell . S.put
 
@@ -80,6 +85,7 @@ halt :: Builder ()
 halt = do
   s <- get
   unless (harmonics s == Low) $ throwError "halt: Harmonics is not low"
+  unless (Set.null (todo s)) $ throwError $ "halt: Model not finished: " ++ show (todo s) 
   unless (length (bots s) == 1) $ throwError "halt: More than one bot still active"
   let bot = head (bots s)
   unless (pos bot == Coord 0 0 0) $ throwError "halt: Bot is not at origin"
@@ -97,13 +103,13 @@ flipHarmonics = do
 
 sMove :: LLD -> Builder ()
 sMove lld@(LLD dim d) = do
-  unless (abs d <= 15) $ throwError "sMove: Distance too big"
+  unless (abs d <= 15) $ throwError ("sMove: Distance too big: " ++ show d)
   s <- get
   let [bot] = bots s
   out $ SMove lld
   put $ s { 
     bots = [ bot { pos = straight dim d (pos bot) } ],
-    energy = energy s + 2 * abs (fromEnum d)
+    energy = energy s + 2 * abs (toInteger d)
   }
 
 lMove :: SLD -> SLD -> Builder ()
@@ -117,7 +123,7 @@ lMove sld1@(SLD dim1 d1) sld2@(SLD dim2 d2) = do
   out $ LMove sld1 sld2
   put $ s { 
     bots = [ bot { pos = straight dim2 d2 $ straight dim1 d1 $ pos bot } ],
-    energy = energy s + 2 * abs (fromEnum d1) + 2 + 2 * abs (fromEnum d2)
+    energy = energy s + 2 * abs (toInteger d1) + 2 + 2 * abs (toInteger d2)
   }
 
 fill :: ND -> Builder ()
@@ -126,14 +132,24 @@ fill nd = do
   let [bot] = bots s
   let c' = addND nd $ pos bot
   unless (c' `Set.notMember` matrix s) $ throwError "fill: Already filled"
+  unless (c' `Set.member` todo s) $ throwError "fill: Not in the model"
   out $ Fill nd
   put $ s {
     matrix = Set.insert c' (matrix s),
+    todo = Set.delete c' (todo s),
     energy = energy s + 12
   }
 
 sMoveLim :: LLD -> Builder ()
-sMoveLim (LLD dim d) = sMove (LLD dim (min 15 d))
+sMoveLim (LLD dim d) = 
+  if d < -15 then sMove (LLD dim (-15)) else 
+  if d > 15 then sMove (LLD dim 15) else sMove (LLD dim d)
+
+lMove' :: SLD -> SLD -> Builder ()
+lMove' (SLD _ 0) (SLD _ 0) = pure ()
+lMove' (SLD _ 0) (SLD dim d) = sMove (LLD dim d)
+lMove' (SLD dim d) (SLD _ 0) = sMove (LLD dim d)
+lMove' s1 s2 = lMove s1 s2
 
 moveTo :: Coord -> Builder ()
 moveTo c'@(Coord x' y' z') = do
@@ -153,19 +169,51 @@ moveTo c'@(Coord x' y' z') = do
     sMoveLim (LLD Z dz)
     moveTo c'
   else if dx == 0 then
-    lMove (SLD Y dy) (SLD Z dz)
+    lMove' (SLD Y dy) (SLD Z dz)
   else if dy == 0 then
-    lMove (SLD X dx) (SLD Z dz)
+    lMove' (SLD X dx) (SLD Z dz)
   else if dz == 0 then
-    lMove (SLD Y dy) (SLD X dx)
+    lMove' (SLD Y dy) (SLD X dx)
   else do
     sMove (LLD Y dy)
-    lMove (SLD Z dz) (SLD Z dz)
+    lMove (SLD X dx) (SLD Z dz)
 
+cross :: [ND]
+cross = [ND 0 (-1) 0, ND 1 (-1) 0, ND 0 (-1) 1, ND (-1) (-1) 0, ND 0 (-1) (-1)]
 
-loadModel :: BS.ByteString -> Model
+crossBelow :: Coord -> Model
+crossBelow c = Set.fromList $ map (\nd -> addND nd c) cross
+
+solveSimpleCross :: Int16 -> Builder ()
+solveSimpleCross r = do
+  let r2 = r `div` 2
+  flipHarmonics
+  
+  for_ [1 .. r] $ \y ->
+    for_ [-r2 .. r2] $ \z'' -> do
+      for_ [-r2 .. r2] $ \x'' -> do
+        let z' = if even y then negate z'' else z''
+        let x' = if even z' then negate x'' else x''
+        let z = z' * 2 - x' + r2
+        let x = x' * 2 + z' + r2
+        -- traceShowM (x, z)
+        s <- get
+        let c = Coord x y z
+        let i = crossBelow c `Set.intersection` todo s
+        unless (Set.null i) $ do
+          moveTo (Coord x y z)
+          for_ i $ fill . (`diffND` c)
+
+  flipHarmonics
+  s <- get
+  let [Bot _ (Coord _ y _) _] = bots s
+  moveTo (Coord 0 y 0)
+  moveTo (Coord 0 0 0)
+  halt
+
+loadModel :: BS.ByteString -> (Int16, Model)
 loadModel bs = 
-  Set.fromList 
+  (r, Set.fromList 
   [ Coord x y z 
   | x <- [0..r - 1]
   , y <- [0..r - 1]
@@ -174,9 +222,8 @@ loadModel bs =
   , let (pw, pb) = p `divMod` 8
   , let b = bs `BS.index` succ pw
   , testBit b pb
-  ]
+  ])
   where
-    r :: Int16
     r = toEnum . fromEnum $ bs `BS.index` 0
 
 printModel :: Model -> IO ()
@@ -188,8 +235,17 @@ printModel model =
       putChar '\n'
     putChar '\n'
 
+load :: String -> IO (Int16, Model)
+load name = loadModel <$> BS.readFile name
+
+save :: String -> S.Put -> IO ()
+save name p = BS.writeFile name (S.runPut p)
+
 main :: IO ()
 main = do
-  contents <- BS.readFile "problemsL/LA001_tgt.mdl"
-  let model = loadModel contents
-  printModel model
+  for_ [1::Int .. 186] $ \n -> do
+    let ns = take (3 - length (show n)) "00" ++ show n
+    putStrLn ns
+    (r, model) <- load ("problemsL/LA" ++ ns ++ "_tgt.mdl")
+    let result = runBuilder model (solveSimpleCross r)
+    either putStrLn (save ("out/LA" ++ ns ++ ".nbt")) result
